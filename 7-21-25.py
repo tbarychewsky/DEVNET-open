99fffa854323625568285f1d46a777aec3ec8981
Tom Barychewsky <tbarychewsky@gmail.com>
2025-07-21 09:20:26 -0400

    commit 4

--- 7-21-25.py
+++ 7-21-25.py
@@ -2,3 +2,7 @@ print ("you are really a slow learner")
 
 print (" Trying to balance all my responsbilities and not waste time is easier said then done ")
 
+print ("Commit 3 need to finish this video series before 11AM")
+
+print ("Commit 4  Need to run at least 5 miles tomorrow morning")
+
